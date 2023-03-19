7692aa96c92e1b909a7061b2feada439271ff090
CarlosMenin <92284071+CarlosMenin@users.noreply.github.com>
2023-03-19 19:03:06 -0300

    Add files via upload

--- Leitura.java
+++ Leitura.java
@@ -1,67 +1,60 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import org.jfugue.player.Player;
-
-
 
 public class Leitura
-{
+{   
+    private static String dados;
+    private static String diretorio;
+    private static String nomeArquivo;
 
-    public static void main(String[] args) throws FileNotFoundException{
-        
-        String diretorio = Get_Directory();
-        List<String> linhas = ReadFile(diretorio);
-        Print_List(linhas);
+    public Leitura(){
+        nomeArquivo = "";
+        diretorio = "";
+        dados = ""; 
     }
 
-    private static void Print_List(List<String> palavras){
-        for(int i = 0; i < palavras.size();i++){
-            System.out.println(palavras.get(i));
-        }
-    }
-
-    private static String Get_Directory(){
+    private static String GetFile(String nomeArquivo){
         String diretorio = System.getProperty("user.dir").concat("\\");
-        Scanner ler_nome = new Scanner(System.in);
-
-        System.out.println("Digite o nome do arquivo\n");
-        String nome_arquivo = ler_nome.nextLine();
-
-        diretorio = diretorio.concat(nome_arquivo);
+        
+        diretorio = diretorio.concat(nomeArquivo);
 
         return diretorio;
         
     }
-    private static List<String> ReadFile(String diretorio) throws FileNotFoundException
+    private static String ReadFile(String diretorio) throws FileNotFoundException
     {
         File file = new File(diretorio);
-        Scanner scan = new Scanner(file);
-        List<String> palavras = new ArrayList<String>();
+        try (Scanner scan = new Scanner(file)) {
+            String palavras = "";
 
-        while(scan.hasNextLine()){
-            palavras.add(scan.nextLine());
+            while(scan.hasNextLine()){
+                palavras = palavras.concat(scan.nextLine());
+            }
+
+            return palavras;
         }
+    }
 
-        return palavras;
+    public static void setFileName(){
+        try (Scanner ler_nome = new Scanner(System.in)) {
+            System.out.println("Digite o nome do arquivo\n");
+            nomeArquivo = ler_nome.nextLine();
+        }
+    }
+    public static void setDados(String diretorio) throws FileNotFoundException{
+        dados = ReadFile(diretorio);
+    }
+    public static void setDirectory(){
+        diretorio = GetFile(nomeArquivo);
+    }
+    public static String getFileName(){
+        return nomeArquivo;
+    }
+    public static String getDados(){
+        return dados;
+    }
+    public static String getDirectory(){
+        return diretorio;
     }
 }  
-
-class Controla_Som{
-    private float volume;
-    private float BPM;
-    private int ID_Instrumento;
-    private boolean mutado;
-
-
-    public Controla_Som(){
-        volume = 50;
-        BPM = 20;
-        ID_Instrumento = 0;
-        mutado = false;
-    }
-
-
-}
